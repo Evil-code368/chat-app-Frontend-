59a223ffe3ac81a3eea894bc60a1772f1621e085
Saket Singh <salets961@gmail.com>
2026-07-05 00:25:20 +0530

    backend url

--- src/Socket.js
+++ src/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-app-frontend-rho-teal.vercel.app", {
+export const socket = io("https://chat-app-backend-api-ok19.onrender.com", {
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
